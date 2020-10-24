64f22f1b0eddf1b06aaf11b9238f7be202489350
Zoltán Szegedi <zoltan.szegedi@it4all.hu>
2020-10-24 10:17:13 +0200

    list all quarries

--- explosions/views.py
+++ explosions/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from explosions.models import Quarry
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    all_quarries = Quarry.objects.all()
+
+    allquarries_text = "<b>All quarries:</b> <br><br>"
+
+    for quarry in all_quarries:
+        allquarries_text += quarry.name + "<br>"
+
+    return HttpResponse(allquarries_text)
