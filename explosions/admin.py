b56705b9bd4603f6d12aecaaae7f8bdfb2cd8b4c
Zoltán Szegedi <zoltan.szegedi@it4all.hu>
2020-10-24 09:57:39 +0200

    db admin

--- explosions/admin.py
+++ explosions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quarry
+
+admin.site.register(Quarry)
--- explosions/models.py
+++ explosions/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Quarry(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = "quarry"
